2cf69a10c1fa535565c2da2f88ca66730045685c
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-05 13:22:04 +0700

    a: Header and footer

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from "../styles/Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; DJ Events 2022</p>
+      <p>
+        <Link href="/">
+          About This Project
+        </Link>
+      </p>
+    </footer>
+  );
+}
