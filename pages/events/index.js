ebc643fe43a81fcd5d769bfd26f0eba09aac5756
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-06 12:02:56 +0700

    a: Displaying events &  m: Api/events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,32 @@
-import Layout from "@/components/Layout"
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/">
+          <a className="btn-secondary">Back</a>
+        </Link>
+      )}
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events:events.slice(0, 6) },
+    revalidate: 1,
+  };
 }
