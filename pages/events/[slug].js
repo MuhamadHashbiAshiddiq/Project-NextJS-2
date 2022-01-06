0a9a00be229db30dafaa60c28c4aad244580e95d
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-06 12:33:20 +0700

    a: getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,4 +1,4 @@
-import Layout from "@/components/Layout"
+import Layout from "@/components/Layout";
 
 export default function EventPage() {
   return (
@@ -6,4 +6,30 @@ export default function EventPage() {
       <h1>My Event</h1>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const res = events.map((map) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: slug }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
 }
